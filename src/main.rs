740d8d54f4b9a1e026e348afff9e38018ebf74da
Danyiel Colin <danyiel5978@gmail.com>
2023-11-08 00:49:35 -0600

    Ch4: introducing logs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use email_newsletter::{configuration::get_configuration, startup::run};
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Can't load config.");
     let connection_pool = PgPool::connect(&configuration.database.connection_url())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,5 @@
 use crate::routes::{health_check::health_check, subscriptions::subscribe};
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -11,6 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone()) // Get a pointer copy
