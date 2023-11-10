4bad86dcc9ba5f78b52336fd6fc023b859cbc95f
Danyiel Colin <danyiel5978@gmail.com>
2023-11-10 05:24:11 -0600

    Ch5: refactor to use PgPoolOptions

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,20 +1,36 @@
 use config::{Config, ConfigError, File, FileFormat};
 use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
 
 #[derive(Deserialize)]
 pub struct Settings {
+    pub application: ApplicationSettings,
     pub database: DatabaseSettings,
-    pub application_port: u16,
+}
+
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
+}
+
+impl ApplicationSettings {
+    pub fn host_address(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
 }
 
 #[derive(Deserialize)]
 pub struct DatabaseSettings {
-    pub username: String,
-    pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub username: String,
+    pub password: Secret<String>,
 }
 
 impl DatabaseSettings {
@@ -37,13 +53,70 @@ impl DatabaseSettings {
             self.port
         ))
     }
+    pub fn without_db(&self) -> PgConnectOptions {
+        PgConnectOptions::new()
+            .username(&self.username)
+            .password(self.password.expose_secret())
+            .host(&self.host)
+            .port(self.port)
+    }
+    pub fn with_db(&self) -> PgConnectOptions {
+        self.without_db().database(&self.database_name)
+    }
+}
+
+fn pathbuf_to_string(path: std::path::PathBuf) -> String {
+    path.into_os_string()
+        .into_string()
+        .expect("Error converting to String")
 }
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
     // Initialize our configuration reader
-    let config = Config::builder()
-        .add_source(File::new("configuration", FileFormat::Yaml))
+    let working_path = std::env::current_dir().expect("Failed to determine the current directory.");
+    let configuration_directory = working_path.join("configuration");
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
+    let base_file = pathbuf_to_string(configuration_directory.join("base"));
+    let env_file = pathbuf_to_string(configuration_directory.join(environment.as_str()));
+
+    let settings = Config::builder()
+        .add_source(File::new(base_file.as_str(), FileFormat::Yaml).required(true))
+        .add_source(File::new(env_file.as_str(), FileFormat::Yaml).required(true))
         .build()?;
 
-    config.try_deserialize::<Settings>()
+    settings.try_deserialize::<Settings>()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `prodution`.",
+                other
+            )),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use email_newsletter::{
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -13,10 +12,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Can't load config.");
-    let connection_pool = PgPool::connect(configuration.database.connection_url().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+    let address = configuration.application.host_address();
     let listener = TcpListener::bind(address)?;
     // let _ = listener.local_addr().unwrap().port();
     run(listener, connection_pool)?.await
